4dca022153d2405d837499f094b67e0532cbe0c0
Asikur Rahman <asikur@bdthemes.com>
2024-03-18 12:05:26 +0600

    license notice style

--- gruntfile.js
+++ gruntfile.js
@@ -80,6 +80,7 @@ module.exports = function (grunt) {
 						expand: true,
 						cwd: 'src/scss/',
 						src: ['admin.scss'],
+
 						dest: 'admin/assets/css/',
 						ext: '.css',
 
@@ -87,6 +88,18 @@ module.exports = function (grunt) {
 							return dest + src.replace(/(.+)\.css$/, 'upk-$1.css');
 						},
 					},
+					{
+						expand: true,
+						cwd: 'src/scss/',
+						src: ['admin-notice.scss'],
+
+						dest: 'admin/assets/css/',
+						ext: '.css',
+
+						rename: function (dest, src) {
+							return dest + src.replace(/(.+)\.css$/, 'upk-$1.css');
+						},
+					}
 				],
 			},
 		},
