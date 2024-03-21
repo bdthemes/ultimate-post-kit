5be9a66c2fd97c5f172361e541a868b1ef41f923
bddevfarid <faridbpi10@gmail.com>
2024-03-21 14:28:29 +0600

    notice fix

--- admin/admin.php
+++ admin/admin.php
@@ -37,9 +37,13 @@ class Admin
 
 		// register_activation_hook(BDTUPK__FILE__, 'install_and_activate');
 
+		add_action('admin_init', [ $this, 'notice_styles' ] );
+
+
+	}
+
+	function notice_styles(){
 		wp_enqueue_style('upk-admin-notice', BDTUPK_ADMIN_ASSETS_URL . 'css/upk-admin-notice.css', [], BDTUPK_VER);
-
-
 	}
 
 
