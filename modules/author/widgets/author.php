9f991a42acc777a6897043ba3dba2bdd1039df17
Maudud Hossain <bdtu027@BDTU020.localdomain>
2024-08-03 17:22:15 +0600

    Author page link issue fixed

--- modules/author/widgets/author.php
+++ modules/author/widgets/author.php
@@ -336,9 +336,9 @@ class Author extends Module_Base {
 		$this->add_control(
 			'exclude',
 			[
-				'label'       => esc_html__('Exclude', 'ultimate-post-kit'),
+				'label'       => esc_html__('Exclude User by ID', 'ultimate-post-kit'),
 				'type'        => Controls_Manager::TEXT,
-				'placeholder' => __('User ID: 1,2', 'ultimate-post-kit'),
+				'placeholder' => __('1,2', 'ultimate-post-kit'),
 			]
 		);
 
@@ -1377,7 +1377,7 @@ class Author extends Module_Base {
 					<div class="upk-item">
 						<?php if ($settings['show_author_avatar']) : ?>
 							<div class="upk-image">
-								<a href="<?php echo get_bloginfo('url') . "/?author=" . esc_attr($author->ID); ?>">
+								<a href="<?php echo get_author_posts_url($author->ID) ?>">
 									<?php echo get_avatar($author->ID, $settings['author_avatar_size']); ?>
 								</a>
 							</div>
