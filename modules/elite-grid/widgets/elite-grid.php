309290a4e02ad11386866470467a184ae187c087
usuf-bdt <usufbdt@gmail.com>
2025-08-20 11:39:53 +0600

    escaping added for ultimate_post_kit_reading_time()

--- modules/elite-grid/widgets/elite-grid.php
+++ modules/elite-grid/widgets/elite-grid.php
@@ -1268,7 +1268,7 @@ class Elite_Grid extends Group_Control_Query {
 								<?php if (_is_upk_pro_activated()) :
 									if ('yes' === $settings['show_reading_time']) : ?>
 										<div class="upk-reading-time" data-separator="<?php echo esc_html($settings['meta_separator']); ?>">
-											<?php echo ultimate_post_kit_reading_time(get_the_content(), $settings['avg_reading_speed']); ?>
+											<?php echo esc_html( ultimate_post_kit_reading_time(get_the_content(), $settings['avg_reading_speed']) ); ?>
 										</div>
 									<?php endif; ?>
 								<?php endif; ?>
@@ -1299,7 +1299,7 @@ class Elite_Grid extends Group_Control_Query {
 									<?php if (_is_upk_pro_activated()) :
 										if ('yes' === $settings['show_reading_time']) : ?>
 											<div class="upk-reading-time" data-separator="<?php echo esc_html($settings['meta_separator']); ?>">
-												<?php echo ultimate_post_kit_reading_time(get_the_content(), $settings['avg_reading_speed']); ?>
+												<?php echo esc_html( ultimate_post_kit_reading_time( get_the_content(), $settings['avg_reading_speed'] ) ); ?>
 											</div>
 										<?php endif; ?>
 									<?php endif; ?>
--- modules/gratis-grid/widgets/gratis-grid.php
+++ modules/gratis-grid/widgets/gratis-grid.php
@@ -1280,7 +1280,7 @@ class Gratis_Grid extends Group_Control_Query {
 								<?php $this->render_date(); ?>
 								<?php if ('yes' === $settings['show_reading_time']) : ?>
 									<div class="upk-reading-time" data-separator="<?php echo esc_html($settings['meta_separator']); ?>">
-										<?php echo ultimate_post_kit_reading_time(get_the_content(), $settings['avg_reading_speed']); ?>
+										<?php echo esc_html( ultimate_post_kit_reading_time( get_the_content(), $settings['avg_reading_speed'] ) ); ?>
 									</div>
 								<?php endif; ?>
 							</div>
