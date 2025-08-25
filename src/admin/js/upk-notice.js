fbc72ef65c08d521f5a6bac9fc33756d6bdc8ef3
Maudud Hossain <86344567+maudud-bdthemes@users.noreply.github.com>
2025-08-25 18:48:12 +0600

    Admin API notice added

--- src/admin/js/upk-notice.js
+++ src/admin/js/upk-notice.js
@@ -149,6 +149,8 @@ jQuery(document).ready(function ($) {
                         // insert right after the <h1> if exists, otherwise at top
                         if ($target.children('hr.wp-header-end').length) {
                             $target.children('hr.wp-header-end').first().after($markup);
+                        } else if ($target.children('h1').length) {
+                            $target.children('h1').first().after($markup);
                         } else {
                             $target.prepend($markup);
                         }
