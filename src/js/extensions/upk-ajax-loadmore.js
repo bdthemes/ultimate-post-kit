d988bb360ae5df256bc859f6e8bdfd5587d4b82d
usuf-bdt <usufbdt@gmail.com>
2025-11-09 18:10:31 +0600

    Ajax Load More feature added

--- src/js/extensions/upk-ajax-loadmore.js
+++ src/js/extensions/upk-ajax-loadmore.js
@@ -162,6 +162,7 @@
             "upk-wixer-grid",
             "upk-tiny-list",
             "upk-scott-list",
+            "upk-buzz-list",
         ];
         
         ajaxLoadMoreWidgets.forEach(widget => {
