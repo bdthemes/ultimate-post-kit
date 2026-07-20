e1577411b733fa7d69353f889c399210034524c6
usuf-bdt <usufbdt@gmail.com>
2026-07-20 18:55:00 +0600

    fix pause on hover control condition logic

--- modules/carbon-slider/widgets/carbon-slider.php
+++ modules/carbon-slider/widgets/carbon-slider.php
@@ -290,8 +290,11 @@ class Carbon_Slider extends Group_Control_Query {
 		$this->add_control(
 			'pauseonhover',
 			[
-				'label' => esc_html__('Pause on Hover', 'ultimate-post-kit'),
-				'type'  => Controls_Manager::SWITCHER,
+				'label'     => esc_html__('Pause on Hover', 'ultimate-post-kit'),
+				'type'      => Controls_Manager::SWITCHER,
+				'condition' => [
+					'autoplay' => 'yes',
+				],
 			]
 		);
 
